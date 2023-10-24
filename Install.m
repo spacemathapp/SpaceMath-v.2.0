9fd8c4e41ab09f4aaff91e1383c1786f0fd7c3f9
spacemathapp <46762837+spacemathapp@users.noreply.github.com>
2023-10-24 00:07:48 -0600

    Update Install.m

--- Install.m
+++ Install.m
@@ -1,9 +1,25 @@
 InstallSpaceMath[] := Module[
-                             {url, localPath}, 
+                             {packageName, url, localPath, PackageLocation, OverwritePackage}, 
+                             WriteString["stdout", "\n Please wait a moment while installing the package SpaceMath 2.0 is performed. \n"];
+                             packageName = "SpaceMath";	
+                             PackageLocation = FileNameJoin[{$UserBaseDirectory, "Paclets", "Repository","SpaceMath-2.0"}];
+                             OverwritePackage = "Looks like SpaceMath 2.0 is already installed. Do you want to replace the current content of " <> PackageLocation <> 
+			                       " with the downloaded version of SpaceMath?";
+			                       If[ DirectoryQ[PackageLocation],
+				                        If[ None, Quiet@DeleteDirectory[PackageLocation, DeleteContents -> True], Null,
+					                         If[ ChoiceDialog[OverwritePackage,{"Yes, overwrite the " <> packageName <>" directory"->True,
+						                           "No, I need to do a backup first. Abort installation."->False}, WindowFloating->True, WindowSize -> {500,120}, WindowTitle->
+						                           "Existing SpaceMath Installation detected"],
+						                           PacletUninstall["SpaceMath"];
+						                           Quiet@DeleteDirectory[PackageLocation, DeleteContents -> True],
+						                           Abort[]
+					                           ]
+				                          ]
+			                         ];
                              url = "https://github.com/spacemathapp/SpaceMath-v.2.0/raw/alpha/BuiltPackage/SpaceMath-2.0.paclet";
                              localPath = FileNameJoin[{$UserDocumentsDirectory, "SpaceMath-2.0.paclet"}];
                              URLDownload[url, localPath];
-                             PacletInstall[FileNameJoin[{$UserDocumentsDirectory, "SpaceMath-2.0.paclet"}]]
+                             PacletInstall[FileNameJoin[{$UserDocumentsDirectory, "SpaceMath-2.0.paclet"}]];
                              WriteString["stdout", "\n Installation complete! Loading SpaceMath 2.0 package ... \n"];
                              Get["SpaceMath`"];
                             ];
